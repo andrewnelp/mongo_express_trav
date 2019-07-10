663daef5e509f6b798d1c8e23356fccd29366b8b
Andrey Nelepov <nelepow@gmail.com>
2019-07-10 15:19:29 -0400

    added delete method-override

--- vidjot/app.js
+++ vidjot/app.js
@@ -115,6 +115,12 @@ app.put("/ideas/:id", (req, res) => {
       res.redirect("/ideas");
     })
   })
+});
+
+// Delete idea
+app.delete("/ideas/:id", (req, res) => {
+  Idea.remove({_id: req.params.id})
+  .then(() => res.redirect("/ideas"));
 })
 
 const port = 5000;

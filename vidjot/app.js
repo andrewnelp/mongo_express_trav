9188450741d6a6d011a829803842a874f6904ee3
Andrey Nelepov <nelepow@gmail.com>
2019-07-10 15:09:48 -0400

    added method-override to edit

--- vidjot/app.js
+++ vidjot/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 
 const app = express();
 
@@ -28,6 +29,9 @@ app.set('view engine', 'handlebars');
 // Body Parser middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
+
+// Method-override middleware
+app.use(methodOverride('_method'))
 
 // Index Route
 app.get('/', (req, res) => {
@@ -96,8 +100,23 @@ app.post("/ideas", (req, res) => {
         res.redirect("/ideas");
       })
   }
+});
 
+// Edit form process
+app.put("/ideas/:id", (req, res) => {
+  Idea.findOne({
+    _id:req.params.id
+  })
+  .then(idea => {
+    // new values
+    idea.title = req.body.title;
+    idea.details = req.body.details;
+    idea.save().then(idea =>{
+      res.redirect("/ideas");
+    })
+  })
 })
+
 const port = 5000;
 
 app.listen(port, () => {
